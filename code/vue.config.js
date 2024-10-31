0e2c3f1e5f74a38b086fb2a699b78d4ee750a361
Pako Díaz <me@pakodiaz.dev>
2024-10-31 00:02:52 -0600

    🔧 [#36] Configure global injection of SASS variables 🌐

--- code/vue.config.js
+++ code/vue.config.js
@@ -1,6 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
+          @import "@/styles/_variables.scss";
+        `
+      }
+    }
+  },
   devServer: {
     allowedHosts: [
       'host.docker.internal',
